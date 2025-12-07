f407d61efe57def2aa45eba88e0026bc931dfc30
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-12-08 01:14:48 +0500

    commit

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -4,13 +4,11 @@ import { protectRoute } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/signup", signup)
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
-router.post("/login", login)
-
-router.post("/logout", logout)
-
-router.put("/update-profile", protectRoute,  updateProfile)
+router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, checkAuth);
 
