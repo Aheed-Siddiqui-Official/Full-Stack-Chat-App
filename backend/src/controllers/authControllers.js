c60324ecd21d353f467951edf06793761d5601eb
MuhammadAheedSiddiqui29 <maheedsiddiqui29@gmail.com>
2025-12-08 01:05:26 +0500

    commit

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -117,6 +117,6 @@ export const checkAuth = (req, res) => {
     res.status(200).json(req.user);
   } catch (error) {
     console.log("Error in checkAuth controller", error.message);
-    res.status(500).json({message:"Internal server error"});
+    res.status(500).json({ message: "Internal server error" });
   }
 };
